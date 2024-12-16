f1c96dd0513d80d518aa147d61cb687046cc6af7
Erwan DELAPORTE <edelapor@sio.lan>
2024-12-16 11:50:24 +0100

    ethan on t'emmerde

--- obj/Debug/net8.0-windows/NemoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/NemoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de673b78bcb2dd92c81a46125936f1400404a305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c8c677e8424e9a2f8116a09f8a518d3503723e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
