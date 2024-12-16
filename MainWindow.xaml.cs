fbdda3eabe0789df61b4b2d1129ddf796074efa9
mateo <mateo.fevrat@free.fr>
2024-12-16 16:40:41 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,35 +20,23 @@ namespace NemoApp
             comboRolePerso.ItemsSource = roles.Values;
             personnels = Connexion.SelectedPersonnel();
             dataGridPersonnel.ItemsSource = personnels;
-            LoadPersonnels();
         }
 
-        private void LoadPersonnels()
-        {
-            // Exemple : charger les personnels dans le DataGrid
-            dataGridPersonnel.ItemsSource = Connexion.SelectedPersonnel(); // Implémentez SelectPersonnels dans la classe Connexion
-        }
         private void AjouterPersonnel_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 // Validation des données d'entrée
-                if (!string.IsNullOrWhiteSpace(txtNomPersonnel.Text) &&
-                    !string.IsNullOrWhiteSpace(txtPrenomPersonnel.Text) &&
-                    comboRolePerso.SelectedItem is ComboBoxItem selectedRole &&
-                    !string.IsNullOrWhiteSpace(txtCertificationPersonnel.Text)
-                   )
-                {
+                
+               
                     string nomPers = txtNomPersonnel.Text;
                     string prePers = txtPrenomPersonnel.Text;
-                    int nomRole = Convert.ToInt32(comboRolePerso);
+                    int nomRole = Convert.ToInt32(comboRolePerso.SelectedValue);
                     string certifRole = txtCertificationPersonnel.Text;
 
                     // Appel de la méthode d'insertion
                     Connexion.InsertPersonnel(nomPers, prePers, nomRole, certifRole);
 
-                    // Recharger les personnels (à implémenter)
-                    LoadPersonnels();
 
                     // Vérification après rechargement
                     if (dataGridPersonnel.Items.Count > 0) // Ajustez selon votre logique
@@ -59,11 +47,10 @@ namespace NemoApp
                     {
                         MessageBox.Show("Erreur : Le personnel n'a pas été ajouté.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
-                }
-                else
-                {
-                    MessageBox.Show("Veuillez remplir tous les champs et entrer un ID valide.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }
+                personnels = Connexion.SelectedPersonnel();
+                dataGridPersonnel.ItemsSource= personnels;
+                dataGridPersonnel.Items.Refresh();
+                
             }
             catch (Exception ex)
             {
