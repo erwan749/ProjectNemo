42291b03a68ddf87f249532c74a1d8ad35f1d6c0
erwan749 <101586302+erwan749@users.noreply.github.com>
2025-01-09 08:48:22 +0100

    Add files via upload

---
+++ Plonge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemoApp
+{
+    internal class Plonge
+    {
+        #region Champs
+
+        private int _idPlonge;
+        private DateTime _datePlonge;
+        private Site _site;
+        private string _duréePlonge;
+
+        #endregion
+
+        #region Constructeur
+
+        public Plonge(int IdPlonge, DateTime DatePlonge, Site Site, string DuréePlonge)
+        {
+            _idPlonge = IdPlonge;
+            _datePlonge = DatePlonge;
+            _site = Site;
+            _duréePlonge = DuréePlonge;
+        }
+
+        #endregion
+
+        #region Accesseurs/Mutateurs
+
+        public int IdPlonge
+        {
+            get { return _idPlonge; }
+            set { _idPlonge = value; }
+        }
+        public DateTime DatePlonge
+        {
+            get { return _datePlonge; }
+            set { _datePlonge = value; }
+        }
+        public Site Site
+        {
+            get { return _site; }
+            set { _site = value; }
+        }
+        public string DuréePlonge
+        {
+            get { return _duréePlonge; }
+            set { _duréePlonge = value; }
+        }
+        #endregion
+
+    }
+}
---
+++ Site.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemoApp
+{
+    internal class Site
+    {
+
+        #region Champs
+
+        private int _idSite;
+        private string _nomSite;
+        private int _profondMax;
+
+        #endregion
+
+        #region Constructeur
+
+        public Site(int IdSite, string NomSite, int ProfondMax)
+        {
+            _idSite = IdSite;
+            _nomSite = NomSite;
+            _profondMax = ProfondMax;
+        }
+
+        #endregion
+
+        #region Accesseurs/Mutateurs
+        public int IdSite
+        {
+            get { return _idSite; }
+            set { _idSite = value; }
+        }
+        public string NomSite
+        {
+            get { return _nomSite; }
+            set { _nomSite = value; }
+        }
+        public int ProfondMax
+        {
+            get { return _profondMax; }
+            set { _profondMax = value; }
+        }
+        #endregion
+    }
+}
