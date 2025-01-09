6e653c314880680cf2ab957fcaa6708a68cd4f8e
Alexandre <alexou.fatet@orange.fr>
2025-01-09 09:14:03 +0100

    Add files via upload

---
+++ Participant.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NemoApp
+{
+    internal class Participant
+    {
+        #region Champs
+        private int _idPart;
+		private Plonge _idPlong;
+		private Client _idCli;
+		private bool _presencePart;
+        #endregion
+
+        #region Accesseurs/Mutateurs
+        public bool PresencePart
+        {
+			get { return _presencePart; }
+			set { _presencePart = value; }
+		}
+
+
+		public Client IdCli
+        {
+			get { return _idCli; }
+			set { _idCli = value; }
+		}
+
+
+		public Plonge IdPlong
+		{
+			get { return _idPlong; }
+			set { _idPlong = value; }
+		}
+
+
+		public int IdPart
+		{
+			get { return _idPart; }
+			set { _idPart = value; }
+		}
+        #endregion
+
+        #region Constructeur
+
+        public Participant(int IdParticipant, Plonge IdPlongée, Client IdClient, int PresenceParticipant)
+		{
+			_idPart = IdParticipant;
+			_idPlong = IdPlongée;
+			_idCli = IdClient;
+			_presencePart = PresenceParticipant == 1;
+		}
+
+        #endregion
+
+    }
+}
